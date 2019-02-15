18d472a9a9561a5465f6d6a42a201a9da22aaa8f
bruno <brunoantunes94@hotmail.com>
2019-02-15 05:03:27 -0200

    Alteração da pagina inicial
    foi separado o arquivo footer e arquivo header para que seja padrão em todas as paginas e alterado o estilo da pagina inicial.

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bruno
+ * Date: 15/02/2019
+ * Time: 03:56
+ */
+?>
+<footer class="container-fluid" style="
+    background-image:url('img/ceu-back-footer.png');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+">
+    <div class="row no-gutters">
+        <div class="col" id="rodape">
+            <img src="img/logo.png" id="logo"/>
+            <h5 id="txtrodape">Rua Dona Ana Chaves, 199 - Centro - Brazópolis - MG.</h5>
+            <h4 id="txtrodape">Fone:(35) 3641-1110 / psaocaetano@yahoo.com.br</h4>
+        </div>
+    </div>
+</footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
