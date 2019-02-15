e637ff2fc55a703c8ad8ace3966f2963cd4838ad
bruno <brunoantunes94@hotmail.com>
2019-02-15 02:22:24 -0200

    Adicionando bootstrap 4.3
    nesta modificação sera onde tudo começa em relação a responsividade do site, e a partir de agora serão modificados os arquivos do site, caso queira voltar o site como era, basta resetar os arquivos antes desse commit

--- index.php
+++ index.php
@@ -4,14 +4,15 @@
         <meta charset="UTF-8">
         <title>Paróquia São Caetano</title>
         <link rel ="stylesheet" type ="text/css" href ="CSS/estilo1.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script language="javascript" type="text/javascript">
             diaNome = new Array("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado");
             mesNome = new Array("janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro");
             now = new Date;
         </script>
     </head>	
-    
+    <body>
         <!-- Piwik -->
         <script type="text/javascript">
             var _paq = _paq || [];
@@ -30,6 +31,7 @@
             })();
         </script>
         <!-- End Piwik Code -->
+
         <nav>
             <ul class="menu">
                 <li><a href="index.php">A Paróquia</a>
@@ -131,5 +133,8 @@
             <h4 id="txtrodape">Rua Dona Ana Chaves, 199 - Centro - Brazópolis - MG.</h4>
             <h3 id="txtrodape">Fone:(35) 3641-1110 / psaocaetano@yahoo.com.br</h3>
         </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
 </html>
